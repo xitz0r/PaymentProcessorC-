68c2e66da85ee7dd31a5146ef519ae128f4baf75
Bruno Paulo <bruno@werules.com>
2016-05-08 02:40:22 -0300

    reorganizing and adapting to nhibernate Student class

---
+++ PaymentProcessor/Entities/Student.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PaymentProcessor.Entities
+{
+    public class Student
+    {
+        public virtual int Id { get; set; }
+        public virtual string Name { get; set; }
+        public virtual string LastName { get; set; }
+        public virtual string CPF { get; set; }
+        public virtual IList<Card> Cards { get; set; }
+        public virtual DateTime Birthday { get; set; }
+        public virtual DateTime DateTimeCreated { get; set; }
+    }
+
+    public class InvalidCPFException : Exception
+    {
+        public InvalidCPFException()
+        {
+        }
+    }
+}
