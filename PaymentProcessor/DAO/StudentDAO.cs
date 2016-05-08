c3350717bebca3f5ad5481f8f9cb78d3ab73a240
Bruno Paulo <bruno@werules.com>
2016-05-08 02:34:45 -0300

    adding StudentDAO

---
+++ PaymentProcessor/DAO/StudentDAO.cs
@@ -0,0 +1,29 @@
+﻿using NHibernate;
+using PaymentProcessor.Entities;
+using PaymentProcessor.Infra;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PaymentProcessor.DAO
+{
+    public class StudentDAO
+    {
+        private ISession session;
+
+
+        public StudentDAO(ISession session)
+        {
+            this.session = session;
+        }
+
+        public void Add(Student student)
+        {
+            ITransaction transacao = session.BeginTransaction();
+            session.Save(student);
+            transacao.Commit();
+        }
+    }
+}
