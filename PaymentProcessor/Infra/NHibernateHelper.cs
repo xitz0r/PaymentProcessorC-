eace62cb2c5fbe4f873351a749ce22aba4a7f32c
Bruno Paulo <bruno@werules.com>
2016-05-08 02:39:20 -0300

    adding NHibernateHelper class

---
+++ PaymentProcessor/Infra/NHibernateHelper.cs
@@ -0,0 +1,42 @@
+﻿using NHibernate;
+using NHibernate.Cfg;
+using NHibernate.Tool.hbm2ddl;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PaymentProcessor.Infra
+{
+    public class NHibernateHelper
+    {
+        private static ISessionFactory fabric = NHibernateHelper.CreateSessionFactory();
+
+
+        public static Configuration RecoverConfiguration()
+        {
+            Configuration cfg = new Configuration();
+            cfg.Configure();
+            return cfg;
+        }
+
+        public static void GenerateSchema()
+        {
+            Configuration cfg = RecoverConfiguration();
+            new SchemaExport(cfg).Create(true, true);
+        }
+
+        public static ISession OpenSession()
+        {
+            return fabric.OpenSession();
+        }
+
+        private static ISessionFactory CreateSessionFactory()
+        {
+            Configuration cfg = NHibernateHelper.RecoverConfiguration();
+            return cfg.BuildSessionFactory();
+        }
+    }
+}
