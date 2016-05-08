d89c1a1538a7eb1d3afe71bebd4ab8bcffefa1de
Bruno Paulo <bruno@werules.com>
2016-05-08 02:39:41 -0300

    adding Card class

---
+++ PaymentProcessor/Entities/Card.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PaymentProcessor.Entities
+{
+    public class Card
+    {
+        public virtual int Id { get; set; }
+        public virtual string PAN { get; set; }
+        public virtual string Track1 { get; set; }
+        public virtual string Track2 { get; set; }
+        public virtual DateTime ExpirationDate { get; set; }
+        public virtual Boolean Blocked { get; set; }
+        public virtual Student Student { get; set; }
+    }
+}
