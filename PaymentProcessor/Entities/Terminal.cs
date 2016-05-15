4679037bfaf3ddeb41cce4c826bea4e70f3d166d
Bruno Paulo <bruno@werules.com>
2016-05-15 16:44:53 -0300

    adding Terminal class and its nhibernate config file

---
+++ PaymentProcessor/Entities/Terminal.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PaymentProcessor.Entities
+{
+    public class Terminal
+    {
+        public virtual int Id { get; set; }
+        public virtual bool IsAdmin { get; set; }
+        public virtual string TerminalId { get; set; }
+        public virtual string Description { get; set; }
+        public virtual IList<Sale> Sales { get; set; }
+    }
+}
