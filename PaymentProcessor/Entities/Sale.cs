9a818e39690a9fd00d6d4e3277f373764250f349
Bruno Paulo <bruno@werules.com>
2016-05-15 16:45:16 -0300

    adding Sale class and its nhibernate config file

---
+++ PaymentProcessor/Entities/Sale.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PaymentProcessor.Entities
+{
+    public class Sale
+    {
+        public virtual int Id { get; set; }
+        public virtual double Value { get; set; }
+        public virtual DateTime DateTimeSale { get; set; }
+        public virtual bool WasRefunded { get; set; }
+        public virtual bool IsReload { get; set; }
+        public virtual Student Student { get; set; }
+        public virtual Terminal Terminal { get; set; }
+    }
+}
