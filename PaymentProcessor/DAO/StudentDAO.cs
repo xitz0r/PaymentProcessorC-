39233cd1f635f36134958e7977aad7edfcc970e9
Bruno Paulo <bruno@werules.com>
2016-05-21 22:28:34 -0300

    adding a lot of 'get' functions to StudentDAO

--- PaymentProcessor/DAO/StudentDAO.cs
+++ PaymentProcessor/DAO/StudentDAO.cs
@@ -1,4 +1,6 @@
 ﻿using NHibernate;
+using NHibernate.Criterion;
+using NHibernate.Linq;
 using PaymentProcessor.Entities;
 using PaymentProcessor.Infra;
 using System;
@@ -24,6 +26,56 @@ namespace PaymentProcessor.DAO
             ITransaction transacao = session.BeginTransaction();
             session.Save(student);
             transacao.Commit();
+        }
+
+        internal int GetTotal()
+        {
+            return this.session.QueryOver<Student>().RowCount();
+        }
+
+        public Student GetFirst()
+        {
+            return this.session.Query<Student>().FirstOrDefault();
+        }
+
+        public Student Get(int id)
+        {
+            ITransaction transaction = session.BeginTransaction();
+            Student student = session.Get<Student>(id);
+            transaction.Commit();
+            return student;
+        }
+
+        public Student GetNextAfter(int id)
+        {
+            Student student = null;
+            ICriteria criteria = session.CreateCriteria<Student>();
+            criteria.Add(Restrictions.Gt("Id", id))
+                    .SetMaxResults(1);
+
+            try    //may not have next student
+            {
+                student = criteria.List<Student>().First<Student>();
+            }
+            catch (Exception) { }
+
+            return student;
+        }
+
+        public Student GetPreviousBefore(int id)
+        {
+            Student student = null;
+            ICriteria criteria = session.CreateCriteria<Student>();
+            criteria.Add(Restrictions.Lt("Id", id))
+                    .SetMaxResults(1);
+
+            try    //may not have previous student
+            {
+                student = criteria.List<Student>().First<Student>();
+            }
+            catch (Exception) { }
+
+            return student;
         }
     }
 }
