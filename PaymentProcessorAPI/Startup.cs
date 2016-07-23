8bccf13b7802f630ce934439840b6d568489c5eb
Bruno Paulo <bruno@werules.com>
2016-07-23 14:32:13 -0300

    server side initial commit

---
+++ PaymentProcessorAPI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PaymentProcessorAPI.Startup))]
+namespace PaymentProcessorAPI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
