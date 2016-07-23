bbe2b18374b6e07d60ad1694f38f531fcceb0abd
Bruno Paulo <bruno@werules.com>
2016-07-23 18:14:20 -0300

    new CardDAO

---
+++ PaymentProcessor/DAO/CardDAO.cs
@@ -0,0 +1,38 @@
+﻿using NHibernate;
+using NHibernate.Criterion;
+using PaymentProcessor.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PaymentProcessor.DAO
+{
+    public class CardDAO
+    {
+        private ISession session;
+
+
+        public CardDAO(ISession session)
+        {
+            this.session = session;
+        }
+
+        public void Add(Card card)
+        {
+            ITransaction transacao = session.BeginTransaction();
+            session.Save(card);
+            transacao.Commit();
+        }
+
+        public Card Get(string pan)
+        {
+            ICriteria criteria = session.CreateCriteria<Card>();
+            if (pan != null)
+                criteria.Add(Restrictions.Eq("PAN", pan));
+
+            return criteria.UniqueResult<Card>();
+        }
+    }
+}
