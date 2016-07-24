0ab482dfc8a6e0abf31133c2801cb86716e0c0d2
Anderson <anderson.moouro@gmail.com>
2016-07-24 00:31:10 -0300

    New studentid form

---
+++ PaymentProcessor/Forms/StudentIDForm.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace PaymentProcessor.Forms
+{
+    partial class StudentIDForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.matAluno = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.okMat = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // matAluno
+            // 
+            this.matAluno.Location = new System.Drawing.Point(12, 53);
+            this.matAluno.Name = "matAluno";
+            this.matAluno.Size = new System.Drawing.Size(100, 20);
+            this.matAluno.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Matrícula:";
+            // 
+            // okMat
+            // 
+            this.okMat.Location = new System.Drawing.Point(12, 79);
+            this.okMat.Name = "okMat";
+            this.okMat.Size = new System.Drawing.Size(75, 23);
+            this.okMat.TabIndex = 2;
+            this.okMat.Text = "OK";
+            this.okMat.UseVisualStyleBackColor = true;
+            this.okMat.Click += new System.EventHandler(this.okMat_Click);
+            // 
+            // StudentIDForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(179, 124);
+            this.Controls.Add(this.okMat);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.matAluno);
+            this.Name = "StudentIDForm";
+            this.Text = "Matricula";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox matAluno;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button okMat;
+    }
+}
---
+++ PaymentProcessor/Forms/StudentIDForm.cs
@@ -0,0 +1,65 @@
+﻿using PaymentProcessor.DAO;
+using PaymentProcessor.Entities;
+using PaymentProcessor.Infra;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PaymentProcessor.Forms
+{
+    public partial class StudentIDForm : Form
+    {
+        public StudentIDForm()
+        {
+            InitializeComponent();
+        }
+
+        private void okMat_Click(object sender, EventArgs e)
+        {
+            if (matAluno.Text != "")
+            {
+                FormCard formCard = new FormCard();
+                DialogResult showFormCard;
+
+                this.Hide();
+                showFormCard = formCard.ShowDialog();
+
+                if (showFormCard == DialogResult.OK)
+                {
+                    StudentDAO studentDAO = new StudentDAO(NHibernateHelper.OpenSession());
+                    Student student = studentDAO.Get(Int32.Parse(matAluno.Text));
+                    if (student == null)
+                        MessageBox.Show("Aluno inexistente");
+                    else
+                    {
+                        Card card = formCard.ReturnValueCard;
+                        CardDAO cardDAO = new CardDAO(NHibernateHelper.OpenSession());
+
+                        if (cardDAO.Get(card.PAN) == null)
+                        {
+                            foreach (Card c in student.Cards)
+                                if (!c.Blocked)
+                                {
+                                    MessageBox.Show("Erro", "Já existe um cartão ativo");
+                                    return;
+                                }
+
+                            student.Cards.Add(card);
+                            cardDAO.Add(card);
+                            studentDAO.Update(student);
+                        }
+                        else
+                            MessageBox.Show("Erro", "Cartão já cadastrado");
+                    }
+                  
+                }
+            }
+        }
+    }
+}
