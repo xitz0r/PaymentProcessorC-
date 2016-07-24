de5b10dab32c44fdf76c358fe6ad548d2fce84ca
Anderson <anderson.moouro@gmail.com>
2016-07-24 19:22:28 -0300

    Título da form de senha modificado para "Senha"

--- PaymentProcessor/Forms/PasswordForm.Designer.cs
+++ PaymentProcessor/Forms/PasswordForm.Designer.cs
@@ -71,7 +71,7 @@
             this.Controls.Add(this.label1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Name = "PasswordForm";
-            this.Text = "PasswordForm";
+            this.Text = "Senha";
             this.ResumeLayout(false);
             this.PerformLayout();
 
