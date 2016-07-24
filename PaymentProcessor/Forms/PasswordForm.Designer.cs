cad5736e83423ee16bb5ec33c3b77f31cefd9424
Anderson <anderson.moouro@gmail.com>
2016-07-24 00:34:06 -0300

    Envio da venda

--- PaymentProcessor/Forms/PasswordForm.Designer.cs
+++ PaymentProcessor/Forms/PasswordForm.Designer.cs
@@ -28,33 +28,57 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pinText = new System.Windows.Forms.TextBox();
+            this.okPin = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // button1
+            // label1
             // 
-            this.button1.Location = new System.Drawing.Point(159, 89);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Desenv";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(25, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(87, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Insira sua senha:";
+            // 
+            // pinText
+            // 
+            this.pinText.Location = new System.Drawing.Point(28, 65);
+            this.pinText.Name = "pinText";
+            this.pinText.Size = new System.Drawing.Size(100, 20);
+            this.pinText.TabIndex = 2;
+            this.pinText.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.pinText.UseSystemPasswordChar = true;
+            // 
+            // okPin
+            // 
+            this.okPin.Location = new System.Drawing.Point(28, 91);
+            this.okPin.Name = "okPin";
+            this.okPin.Size = new System.Drawing.Size(75, 23);
+            this.okPin.TabIndex = 0;
+            this.okPin.Text = "OK";
+            this.okPin.Click += new System.EventHandler(this.okPin_Click);
             // 
             // PasswordForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(242, 177);
+            this.Controls.Add(this.okPin);
+            this.Controls.Add(this.pinText);
+            this.Controls.Add(this.label1);
             this.Name = "PasswordForm";
             this.Text = "PasswordForm";
+            this.Load += new System.EventHandler(this.PasswordForm_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox pinText;
+        private System.Windows.Forms.Button okPin;
     }
 }
