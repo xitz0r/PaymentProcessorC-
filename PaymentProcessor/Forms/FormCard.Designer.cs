742afec07120e4971624a72c9fdbb09284ffeca6
Anderson <anderson.moouro@gmail.com>
2016-07-24 00:06:52 -0300

    Ajuste layout.

--- PaymentProcessor/Forms/FormCard.Designer.cs
+++ PaymentProcessor/Forms/FormCard.Designer.cs
@@ -30,7 +30,6 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormCard));
             this.label2 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label2
@@ -38,19 +37,9 @@
             this.label2.AutoSize = true;
             this.label2.Location = new System.Drawing.Point(28, 216);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.Size = new System.Drawing.Size(0, 13);
             this.label2.TabIndex = 1;
-            this.label2.Text = "label2";
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(271, 27);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 2;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.label2.Visible = false;
             // 
             // FormCard
             // 
@@ -59,7 +48,6 @@
             this.AutoSize = true;
             this.BackgroundImage = global::PaymentProcessor.Properties.Resources.swipe_card;
             this.ClientSize = new System.Drawing.Size(384, 269);
-            this.Controls.Add(this.button1);
             this.Controls.Add(this.label2);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
@@ -77,6 +65,5 @@
 
         #endregion
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Button button1;
     }
 }
