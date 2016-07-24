b7dc2237df45f0689bba9437a57df0c399af61cc
Bruno Paulo <bruno@werules.com>
2016-07-24 15:21:00 -0300

    closing nhibernate sessions in studentIdForm

--- PaymentProcessor/Forms/StudentIDForm.cs
+++ PaymentProcessor/Forms/StudentIDForm.cs
@@ -1,4 +1,5 @@
-﻿using PaymentProcessor.DAO;
+﻿using NHibernate;
+using PaymentProcessor.DAO;
 using PaymentProcessor.Entities;
 using PaymentProcessor.Infra;
 using System;
@@ -32,14 +33,15 @@ namespace PaymentProcessor.Forms
 
                 if (showFormCard == DialogResult.OK)
                 {
-                    StudentDAO studentDAO = new StudentDAO(NHibernateHelper.OpenSession());
+                    ISession session = NHibernateHelper.OpenSession();
+                    StudentDAO studentDAO = new StudentDAO(session);
                     Student student = studentDAO.Get(Int32.Parse(matAluno.Text));
                     if (student == null)
                         MessageBox.Show("Aluno inexistente");
                     else
                     {
                         Card card = formCard.ReturnValueCard;
-                        CardDAO cardDAO = new CardDAO(NHibernateHelper.OpenSession());
+                        CardDAO cardDAO = new CardDAO(session);
 
                         if (cardDAO.Get(card.PAN) == null)
                         {
@@ -57,7 +59,7 @@ namespace PaymentProcessor.Forms
                         else
                             MessageBox.Show("Erro", "Cartão já cadastrado");
                     }
-
+                    session.Close();
                 }
             }
         }
@@ -74,14 +76,15 @@ namespace PaymentProcessor.Forms
 
                 if (showFormCard == DialogResult.OK)
                 {
-                    StudentDAO studentDAO = new StudentDAO(NHibernateHelper.OpenSession());
+                    ISession session = NHibernateHelper.OpenSession();
+                    StudentDAO studentDAO = new StudentDAO(session);
                     Student student = studentDAO.Get(Int32.Parse(matAluno.Text));
                     if (student == null)
                         MessageBox.Show("Aluno inexistente");
                     else
                     {
                         Card card = formCard.ReturnValueCard;
-                        CardDAO cardDAO = new CardDAO(NHibernateHelper.OpenSession());
+                        CardDAO cardDAO = new CardDAO(session);
                         foreach (Card c in student.Cards)
                             if (!c.Blocked)
                             {
@@ -91,6 +94,7 @@ namespace PaymentProcessor.Forms
                             }
                         MessageBox.Show("Não há cartões ativos para esse aluno.");
                     }
+                    session.Close();
                 }
             }
         }
