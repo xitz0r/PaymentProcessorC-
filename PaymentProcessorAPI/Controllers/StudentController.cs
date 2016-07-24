cb23fb0f5de51ed790cd72a8464a602294b3c683
Bruno Paulo <bruno@werules.com>
2016-07-24 19:00:30 -0300

    new student balance endpoint

---
+++ PaymentProcessorAPI/Controllers/StudentController.cs
@@ -0,0 +1,39 @@
+﻿using NHibernate;
+using PaymentProcessor.DAO;
+using PaymentProcessor.Entities;
+using PaymentProcessor.Infra;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace PaymentProcessorAPI.Controllers
+{
+    public class StudentController : ApiController
+    {
+        public HttpResponseMessage Get([FromUri] int id)
+        {
+            ISession session = NHibernateHelper.OpenSession();
+            StudentDAO studentDAO = new StudentDAO(session);
+            Student student = studentDAO.Get(id);
+
+            if (student == null)
+                return ReturnResponse(session, HttpStatusCode.NotFound);
+            else
+                return ReturnResponse(session, HttpStatusCode.OK, student.Balance);  
+        }
+
+        internal HttpResponseMessage ReturnResponse(ISession session, HttpStatusCode code, double balance=-1)
+        {
+            if (session != null)
+                session.Close();
+
+            if (balance >= 0)
+                return Request.CreateResponse(code, balance);
+            else
+                return Request.CreateResponse(code);
+        }
+    }
+}
