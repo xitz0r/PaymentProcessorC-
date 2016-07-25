e9c72ecd751b97bbdaa32e2838c6ac8673d452c2
Anderson <anderson.moouro@gmail.com>
2016-07-24 23:45:03 -0300

    Correção no bloqueio de cartão.

--- PaymentProcessor/Forms/StudentIDForm.cs
+++ PaymentProcessor/Forms/StudentIDForm.cs
@@ -67,36 +67,30 @@ namespace PaymentProcessor.Forms
 
         private void button1_Click(object sender, EventArgs e)
         {
+            Boolean bFlag = false;
             if (matAluno.Text != "")
             {
-                FormCard formCard = new FormCard();
-                DialogResult showFormCard;
-
                 this.Hide();
-                showFormCard = formCard.ShowDialog();
-
-                if (showFormCard == DialogResult.OK)
+                
+                ISession session = NHibernateHelper.OpenSession();
+                StudentDAO studentDAO = new StudentDAO(session);
+                Student student = studentDAO.Get(Int32.Parse(matAluno.Text));
+                if (student == null)
+                    MessageBox.Show("Aluno inexistente");
+                else
                 {
-                    ISession session = NHibernateHelper.OpenSession();
-                    StudentDAO studentDAO = new StudentDAO(session);
-                    Student student = studentDAO.Get(Int32.Parse(matAluno.Text));
-                    if (student == null)
-                        MessageBox.Show("Aluno inexistente");
-                    else
-                    {
-                        Card card = formCard.ReturnValueCard;
-                        CardDAO cardDAO = new CardDAO(session);
-                        foreach (Card c in student.Cards)
-                            if (!c.Blocked)
-                            {
-                                c.Blocked = true;
-                                cardDAO.Update(c);
-                                MessageBox.Show("Cartão bloqueado com sucesso.");
-                            }
-                        MessageBox.Show("Não há cartões ativos para esse aluno.");
-                    }
-                    session.Close();
+                    CardDAO cardDAO = new CardDAO(session);
+                    foreach (Card c in student.Cards)
+                        if (!c.Blocked)
+                        {
+                            bFlag = true;
+                            c.Blocked = true;
+                            cardDAO.Update(c);
+                        }
+                    MessageBox.Show((bFlag? "Cartão bloqueado com sucesso.":"Não há cartões ativos para esse aluno."), "Bloquear cartão");
                 }
+                session.Close();
+                
             }
         }
     }
